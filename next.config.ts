d36d8cc14ed730d671e2739cc101ea22a7ab1fc2
Divy Parekh <divyparekh1810@gmail.com>
2025-08-20 23:17:46 +0530

    Add image remote patterns to Next.js configuration
    - Updated next.config.ts to include remote image patterns for animated images from specified hostnames: animatedimages.org, cameronsworld.net, web.archive.org, and blob.gifcities.org.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.animatedimages.org",
+      },
+      {
+        protocol: "https",
+        hostname: "www.cameronsworld.net",
+      },
+      {
+        protocol: "https",
+        hostname: "web.archive.org",
+      },
+      {
+        protocol: "https",
+        hostname: "blob.gifcities.org"
+      }
+    ],
+  },
 };
 
 export default nextConfig;
